ead92f2d05fd69146069949ab206a9494833db2f
siikamiika <siikamiika@users.noreply.github.com>
2016-03-15 23:05:01 +0200

    JMdict: show wasei correctly

--- server.py
+++ server.py
@@ -205,7 +205,9 @@ class JMdict_e(object):
                 field=[[field, ent[field]] for field in _get_tags('field')],
                 misc=[[misc, ent[misc]] for misc in _get_tags('misc')],
                 s_inf=_get_tags('s_inf'),
-                lsource=[[ls.attrib.get('{http://www.w3.org/XML/1998/namespace}lang') or 'eng', ls.text]
+                lsource=[[ls.attrib.get('{http://www.w3.org/XML/1998/namespace}lang') or
+                        (ls.attrib.get('ls_wasei') and 'wasei') or 'eng',
+                        ls.text]
                     for ls in sense.iter('lsource')],
                 dial=[[dial, ent[dial]] for dial in _get_tags('dial')],
             )
