siikamiika/mecab-translate

commit 1aed5ade28fffa385c8a10ded9b9c610efffd9e1

client: smart translation sorting
When sorting, prefer translations that have the same part of speech as the
query.

client/controllers/translation.js
@@ -37,7 +37,71 @@ angular.module('mecab-translate')
                 entry.common = common;
             });
 
-            return Helpers.commonSort(a, b);
+            var commonSort = function (a, b) {
+                if ((a.common && b.common) || (!a.common && !b.common)) {
+                    return 0;
+                }
+                else if (b.common) {
+                    return 1;
+                }
+                else {
+                    return -1;
+                }
+            }
+
+            var posSort = function (a, b) {
+                if (!$scope.query.pos) {
+                    return commonSort(a, b);
+                }
+                var pos = Helpers.mecabToEdictPos($scope.query.pos) || [];
+                var aPos = Helpers.intersection(pos, [].concat.apply([], a.translations.map(function(t){
+                    return t.pos.map(function(p) {
+                        return p[0];
+                    });
+                }))).length;
+                var bPos = Helpers.intersection(pos, [].concat.apply([], b.translations.map(function(t){
+                    return t.pos.map(function(p) {
+                        return p[0];
+                    });
+                }))).length;
+                if ((aPos && bPos) || (!aPos && !bPos)) {
+                    return readingSort(a, b);
+                }
+                else if (bPos) {
+                    return 1;
+                }
+                else {
+                    return -1;
+                }
+            }
+
+            var readingSort = function(a, b) {
+                if (!$scope.query.lemma_reading) {
+                    return commonSort(a, b);
+                }
+                var lemmaReadingHira = Helpers.kataToHira($scope.query.lemma_reading);
+                var aReadings = a.readings.map(function(r) {
+                    return r.text;
+                });
+                var bReadings = b.readings.map(function(r) {
+                    return r.text;
+                });
+
+                a.hasReading = aReadings.indexOf(lemmaReadingHira) >= 0 || aReadings.indexOf($scope.query.lemma_reading) >= 0;
+                b.hasReading = bReadings.indexOf(lemmaReadingHira) >= 0|| bReadings.indexOf($scope.query.lemma_reading) >= 0;
+
+                if ((a.hasReading && b.hasReading) || (!a.hasReading && !b.hasReading)) {
+                    return commonSort(a, b);
+                }
+                else if (b.hasReading) {
+                    return 1;
+                }
+                else {
+                    return -1;
+                }
+            }
+
+            return posSort(a, b);
 
         });
     }

client/index.html
@@ -208,7 +208,7 @@
         <hr>
         <!-- add more -->
         <div>
-            <button ng-if="!phraseExampleButtonClicked &amp;&amp; query" ng-click="getPhraseExamples()">Examples for {{query}}</button>
+            <button ng-if="!phraseExampleButtonClicked &amp;&amp; query" ng-click="getPhraseExamples()">Examples for {{query.lemma ? query.lemma : query}}</button>
             <ul>
                 <li ng-repeat="example in phraseExamples">
                     <div ng-if="example.message">{{example.message}}</div>
@@ -230,7 +230,7 @@
         </div>
         <hr>
         <div>
-            <button ng-click="showLongerEntries()" ng-if="longerEntries.length">More words starting with {{query}} ({{longerEntries.length}})</button>
+            <button ng-click="showLongerEntries()" ng-if="longerEntries.length">More words starting with {{query.lemma ? query.lemma : query}} ({{longerEntries.length}})</button>
             <div ng-repeat="entry in longerEntryListing"><span class="clickable" ng-click="translate(entry)">{{entry}}</span></div>
         </div>
     </div>

client/controllers/output.js
@@ -22,7 +22,7 @@ angular.module('mecab-translate')
     $scope.showWordInfo = function(word) {
         if (!window.getSelection().toString()) {
             $scope.word = word;
-            JMdict_e.translate((word.lemma||'').split('-')[0] || word.literal);
+            JMdict_e.translate(word, true);
             Phrase.customOutput([]);
         }
     }

client/services/helpers.js
@@ -24,17 +24,58 @@ angular.module('mecab-translate')
         KEIYOUSHI: '形容詞'
     }
 
+    var mecabToEdictPos = {};
+    mecabToEdictPos[mecabPos.JOSHI] = ['prt', 'aux'];
+    mecabToEdictPos[mecabPos.JODOUSHI] = ['aux-v'];
+    mecabToEdictPos[mecabPos.SETSUBIJI] = ['suf'];
+    mecabToEdictPos[mecabPos.SETTOUJI] = ['pref'];
+
+    var HIRAGANA_START = 0x3041;
+    var HIRAGANA_END = 0x3096;
+    var KATAKANA_START = 0x30A1;
+    var KATAKANA_END = 0x30FA;
+
+    var between = function(n, a, b) {
+        return n >= a && n <= b;
+    }
+
+    var isHira = function(c) {
+        return between(c.charCodeAt(0), HIRAGANA_START, HIRAGANA_END);
+    }
+
+    var isKata = function(c) {
+        return between(c.charCodeAt(0), KATAKANA_START, KATAKANA_END);
+    }
+
     return {
-        commonSort: function(a, b) {
-            if ((a.common && b.common) || (!a.common && !b.common)) {
-                return 0;
-            }
-            else if (b.common) {
-                return 1;
-            }
-            else {
-                return -1;
-            }
+        mecabToEdictPos: function(pos) {
+            return mecabToEdictPos[pos];
+        },
+        kataToHira: function(kata) {
+            var hira = [];
+            kata.split('').forEach(function(k) {
+                if (isKata(k)) {
+                    var code = k.charCodeAt(0);
+                    code += HIRAGANA_START - KATAKANA_START;
+                    hira.push(String.fromCharCode(code));
+                } else {
+                    hira.push(k);
+                }
+            });
+            return hira.join('');
+        },
+        hiraToKata: function(hira) {
+            var kata = [];
+            hira.split('').forEach(function(h) {
+                if (isHira(h)) {
+                    var code = h.charCodeAt(0);
+                    code += HIRAGANA_START - KATAKANA_START;
+                    kata.push(String.fromCharCode(code));
+                } else {
+                    kata.push(h);
+                }
+            });
+            return kata.join('');
         },
         posClass: function(pos) {
             switch(pos) {

client/services/jmdict_e.js
@@ -5,12 +5,14 @@ angular.module('mecab-translate')
     var last;
 
     return {
-        translate: function(val) {
-            last = val;
+        translate: function(word, lemma) {
+            last = word;
+            if (lemma)
+                word = (word.lemma||'').split('-')[0] || word.literal;
             $http({
                 method: 'GET',
                 url: '/jmdict_e',
-                params: {query: val}
+                params: {query: word}
             }).then(function success(data) {
                 output(data.data);
             }, function error(data) {

server.py
@@ -129,7 +129,7 @@ class Mecab(object):
         'inflection_form', 'lemma', 'reading', 'hatsuon']
 
     UNIDIC = ['pos', 'pos2', 'pos3', 'pos4', 'inflection_type',
-        'inflection_form', '_', 'lemma', '_', 'reading']
+        'inflection_form', 'lemma_reading', 'lemma', '_', 'reading']
 
     def __init__(self):
         self.dictionary_format = Mecab.IPADIC

commit 809fadfd3b3d22a06a1a07fa22ccd7d05efb403e

fix phrase lookup
broken by 1aed5ade28fffa385c8a10ded9b9c610efffd9e1

client/controllers/translation.js
@@ -5,6 +5,10 @@ angular.module('mecab-translate')
         return Helpers.intersection(Helpers.commonPriority, word.pri).length;
     }
 
+    $scope.queryAsString = function() {
+        return $scope.query.lemma || $scope.query.literal || $scope.query;
+    }
+
     $scope.setEntries = function(entries) {
 
         entries.forEach(function(entry) {
@@ -130,7 +134,7 @@ angular.module('mecab-translate')
         output.forEach(function(example) {
             if (!example.jpn)
                 return;
-            example.jpn = example.jpn.split(new RegExp('('+$scope.query+')', 'g')).map(function(part) {
+            example.jpn = example.jpn.split(new RegExp('('+$scope.queryAsString()+')', 'g')).map(function(part) {
                 return part;
             });
         });
@@ -142,14 +146,15 @@ angular.module('mecab-translate')
 
     $scope.getPhraseExamples = function(more, previous) {
         Phrase.customOutput([{message: 'Searching...'}]);
+        var q = $scope.queryAsString();
         if (previous) {
             $scope.phraseExampleStart -= 20;
-            Phrase.search($scope.query, 20, $scope.phraseExampleStart, false);
+            Phrase.search(q, 20, $scope.phraseExampleStart, false);
         } else if (more) {
-            Phrase.search($scope.query, 20, $scope.phraseExampleStart, false);
+            Phrase.search(q, 20, $scope.phraseExampleStart, false);
             $scope.phraseExampleStart += 20;
         } else {
-            Phrase.search($scope.query, 5, 0, true);
+            Phrase.search(q, 5, 0, true);
         }
         $scope.phraseExampleButtonClicked = true;
     }

client/index.html
@@ -208,12 +208,12 @@
         <hr>
         <!-- add more -->
         <div>
-            <button ng-if="!phraseExampleButtonClicked &amp;&amp; query" ng-click="getPhraseExamples()">Examples for {{query.lemma ? query.lemma : query}}</button>
+            <button ng-if="!phraseExampleButtonClicked &amp;&amp; query" ng-click="getPhraseExamples()">Examples for {{queryAsString()}}</button>
             <ul>
                 <li ng-repeat="example in phraseExamples">
                     <div ng-if="example.message">{{example.message}}</div>
                     <div style="font-size: 24px;">
-                        <span ng-repeat="part in example.jpn track by $index" ng-style="{'color': part == query ? 'red' : ''}">{{part}}</span>
+                        <span ng-repeat="part in example.jpn track by $index" ng-style="{'color': part == queryAsString() ? 'red' : ''}">{{part}}</span>
                     </div>
                     <div>{{example.eng}}</div>
                     <hr ng-if="!$last">