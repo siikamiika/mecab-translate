siikamiika/mecab-translate

commit d2da5287b9bf249d4bd7b7e2df8206015553ce98

sort jmdict_e results server-side
Also remove phrase lookup due to long lookup times and relatively low use

client/controllers/translation.js
@@ -1,12 +1,8 @@
 angular.module('mecab-translate')
-.controller('Translation', function($scope, JMdict_e, Tatoeba, Phrase, Kanjidic2, SimilarKanji, KanjiVG, Helpers) {
-
-    $scope.isCommon = function(word) {
-        return Helpers.intersection(Helpers.commonPriority, word.pri).length;
-    }
+.controller('Translation', function($scope, JMdict_e, Tatoeba, Kanjidic2, SimilarKanji, KanjiVG, Helpers) {
 
     $scope.queryAsString = function() {
-        return $scope.query.lemma || $scope.query.literal || $scope.query;
+        return $scope.query.lemma.split('-')[0] || $scope.query.literal || $scope.query;
     }
 
     $scope.setEntries = function(entries) {
@@ -22,92 +18,8 @@ angular.module('mecab-translate')
             });
         });
 
-        $scope.entries = entries.sort(function(a, b) {
+        $scope.entries = entries;
 
-            [a, b].forEach(function(entry) {
-                var common = false;
-                for (var i in entry.words) {
-                    if ($scope.isCommon(entry.words[i])) {
-                        common = true;
-                        break;
-                    }
-                }
-                for (var i in entry.readings) {
-                    if ($scope.isCommon(entry.readings[i])) {
-                        common = true;
-                        break;
-                    }
-                }
-                entry.common = common;
-            });
-
-            var commonSort = function (a, b) {
-                if ((a.common && b.common) || (!a.common && !b.common)) {
-                    return 0;
-                }
-                else if (b.common) {
-                    return 1;
-                }
-                else {
-                    return -1;
-                }
-            }
-
-            var posSort = function (a, b) {
-                if (!$scope.query.pos) {
-                    return 0;
-                }
-                var pos = Helpers.mecabToEdictPos($scope.query.pos) || [];
-                var aPos = Helpers.intersection(pos, [].concat.apply([], a.translations.map(function(t){
-                    return t.pos.map(function(p) {
-                        return p[0];
-                    });
-                }))).length;
-                var bPos = Helpers.intersection(pos, [].concat.apply([], b.translations.map(function(t){
-                    return t.pos.map(function(p) {
-                        return p[0];
-                    });
-                }))).length;
-                if ((aPos && bPos) || (!aPos && !bPos)) {
-                    return 0;
-                }
-                else if (bPos) {
-                    return 1;
-                }
-                else {
-                    return -1;
-                }
-            }
-
-            var readingSort = function(a, b) {
-                if (!$scope.query.lemma_reading) {
-                    return 0;
-                }
-                var lemmaReadingHira = Helpers.kataToHira($scope.query.lemma_reading);
-                var aReadings = a.readings.map(function(r) {
-                    return r.text;
-                });
-                var bReadings = b.readings.map(function(r) {
-                    return r.text;
-                });
-
-                aHasReading = aReadings.indexOf(lemmaReadingHira) >= 0 || aReadings.indexOf($scope.query.lemma_reading) >= 0;
-                bHasReading = bReadings.indexOf(lemmaReadingHira) >= 0|| bReadings.indexOf($scope.query.lemma_reading) >= 0;
-
-                if ((aHasReading && bHasReading) || (!aHasReading && !bHasReading)) {
-                    return 0;
-                }
-                else if (bHasReading) {
-                    return 1;
-                }
-                else {
-                    return -1;
-                }
-            }
-
-            return readingSort(a, b) || posSort(a, b) || commonSort(a, b);
-
-        });
     }
 
     $scope.showLongerEntries = function() {
@@ -117,8 +29,6 @@ angular.module('mecab-translate')
 
     JMdict_e.setOutput(function(output) {
         $scope.query = JMdict_e.getLast();
-        $scope.phraseExampleButtonClicked = false;
-        $scope.phraseExampleStart = 0;
         $scope.longerEntryListing = [];
         $scope.regexResults = [];
         $scope.shorterEntries = output.shorter;
@@ -134,41 +44,20 @@ angular.module('mecab-translate')
         }
     });
 
-    Phrase.setOutput(function(output) {
-        output.forEach(function(example) {
-            if (!example.jpn)
-                return;
-            example.jpn = example.jpn.split(new RegExp('('+$scope.queryAsString()+')', 'g')).map(function(part) {
-                return part;
-            });
-        });
-        $scope.phraseExamples = output;
-    });
-
-    $scope.phraseExampleButtonClicked = false;
-    $scope.phraseExampleStart = 0;
-
-    $scope.getPhraseExamples = function(more, previous) {
-        Phrase.customOutput([{message: 'Searching...'}]);
-        var q = $scope.queryAsString();
-        if (previous) {
-            $scope.phraseExampleStart -= 20;
-            Phrase.search(q, 20, $scope.phraseExampleStart, false);
-        } else if (more) {
-            Phrase.search(q, 20, $scope.phraseExampleStart, false);
-            $scope.phraseExampleStart += 20;
-        } else {
-            Phrase.search(q, 5, 0, true);
-        }
-        $scope.phraseExampleButtonClicked = true;
-    }
-
     $scope.translate = function(text) {
-        JMdict_e.translate(text);
+        var obj = false;
+        if (typeof text == 'object') {
+            obj = true;
+            text = {
+                lemma: text[0],
+                reading: isNaN(text[1]) ? text[1] : null
+            }
+        }
+        JMdict_e.translate(text, obj);
     }
 
     $scope.parseReference = function(ref) {
-        return ref.split('・')[0];
+        return ref.split('・');
     }
 
     $scope.getKanjidic2 = function(kanji) {

client/index.html
@@ -215,7 +215,7 @@
                 <div style="font-size: 28px">
                     <span ng-repeat="word in entry.words">
                         <span
-                            ng-class="{'common': isCommon(word)}"
+                            ng-class="{'common': word.common}"
                             ng-attr-title="{{word.pri.join(', ')}}"
                         ><span ng-repeat="char in word.text track by $index" class="clickable" ng-click="setKanjivgChar(char); getKanjidic2(char); getSimilarKanji(char)">{{char}}</span></span><span
                             ng-if="word.inf.length"
@@ -229,7 +229,7 @@
                     <span ng-repeat="reading in entry.readings">
                         <span
                             class="clickable"
-                            ng-class="{'common': isCommon(reading)}"
+                            ng-class="{'common': reading.common}"
                             ng-attr-title="{{reading.pri.join(', ')}}"
                             ng-click="translate(reading.text)"
                         >{{reading.text}}</span><span
@@ -287,29 +287,7 @@
         </ul>
         <hr>
         <div>
-            <button ng-if="!phraseExampleButtonClicked &amp;&amp; query" ng-click="getPhraseExamples()">Examples for {{queryAsString()}}</button>
-            <ul>
-                <li ng-repeat="example in phraseExamples">
-                    <div ng-if="example.message">{{example.message}}</div>
-                    <div style="font-size: 24px;">
-                        <span ng-repeat="part in example.jpn track by $index" ng-style="{'color': part == queryAsString() ? 'red' : ''}">{{part}}</span>
-                    </div>
-                    <div>{{example.eng}}</div>
-                    <hr ng-if="!$last">
-                </li>
-            </ul>
-            <button
-                ng-if="phraseExampleStart >= 40"
-                ng-click="getPhraseExamples(true, true)"
-            >Previous 20</button>
-            <button
-                ng-if="phraseExampleButtonClicked &amp;&amp; phraseExamples.length >= 5"
-                ng-click="getPhraseExamples(true)"
-                >{{phraseExamples.length == 5 ? 'More' : 'Next 20'}}</button>
-        </div>
-        <hr>
-        <div>
-            <button ng-click="showLongerEntries()" ng-if="longerEntries.length">More words starting with {{query.lemma ? query.lemma : query}} ({{longerEntries.length}})</button>
+            <button ng-click="showLongerEntries()" ng-if="longerEntries.length">More words starting with {{queryAsString()}} ({{longerEntries.length}})</button>
             <div ng-repeat="entry in longerEntryListing"><span class="clickable" ng-click="translate(entry)">{{entry}}</span></div>
         </div>
     </div>
@@ -325,7 +303,6 @@
 <script src="services/kanjidic2.js"></script>
 <script src="services/kanjivg.js"></script>
 <script src="services/tatoeba.js"></script>
-<script src="services/phrase.js"></script>
 <script src="services/kanjivgparts.js"></script>
 <script src="services/similarkanji.js"></script>
 <script src="services/responsivevoice.js"></script>

client/controllers/output.js
@@ -1,5 +1,5 @@
 angular.module('mecab-translate')
-.controller('Output', function($scope, Mecab, JMdict_e, Kanjidic2, KanjiVG, KanjiVGParts, SimilarKanji, ResponsiveVoice, RemoteTts, Tts, TtsEvents, Phrase, EventBridge, Config, Helpers) {
+.controller('Output', function($scope, Mecab, JMdict_e, Kanjidic2, KanjiVG, KanjiVGParts, SimilarKanji, ResponsiveVoice, RemoteTts, Tts, TtsEvents, EventBridge, Config, Helpers) {
 
     $scope.posClass = Helpers.posClass;
 
@@ -41,7 +41,6 @@ angular.module('mecab-translate')
         if (!window.getSelection().toString()) {
             $scope.word = word;
             JMdict_e.translate(word, true);
-            Phrase.customOutput([]);
         }
     }
 
@@ -54,7 +53,6 @@ angular.module('mecab-translate')
         if (selection) {
             JMdict_e.translate(selection);
             $scope.TTS(selection);
-            Phrase.customOutput([]);
         }
     }
 

client/services/helpers.js
@@ -108,7 +108,7 @@ angular.module('mecab-translate')
             hira.split('').forEach(function(h) {
                 if (isHira(h)) {
                     var code = h.charCodeAt(0);
-                    code += HIRAGANA_START - KATAKANA_START;
+                    code += KATAKANA_START - HIRAGANA_START;
                     kata.push(String.fromCharCode(code));
                 } else {
                     kata.push(h);
@@ -180,13 +180,6 @@ angular.module('mecab-translate')
                 }
             });
         },
-        commonPriority: [
-            'news1',
-            'ichi1',
-            'spec1',
-            'spec2',
-            'gai1'
-        ],
         blend: function(color1, color2, weight) {
             var outputColor = [];
 

client/services/jmdict_e.js
@@ -1,20 +1,27 @@
 angular.module('mecab-translate')
-.factory('JMdict_e', function($http) {
+.factory('JMdict_e', function($http, Helpers) {
 
     var output;
     var last;
 
     return {
-        translate: function(word, lemma, regex) {
+        translate: function(word, obj, regex) {
             last = word;
-            if (lemma)
+            var reading, pos;
+            if (obj) {
+                reading = word.reading;
+                pos = Helpers.mecabToEdictPos(word.pos) || [];
+                pos = pos.join(',');
                 word = (word.lemma||'').split('-')[0] || word.literal;
+            }
             $http({
                 method: 'GET',
                 url: '/jmdict_e',
                 params: {
                     query: word,
-                    regex: regex ? 'yes' : 'no'
+                    regex: regex ? 'yes' : 'no',
+                    reading: reading,
+                    pos: pos
                 }
             }).then(function success(data) {
                 output(data.data);

client/services/phrase.js
@@ -1,33 +1,0 @@
-angular.module('mecab-translate')
-.factory('Phrase', function($http) {
-    
-    var output;
-
-    return {
-        search: function(phrase, max, start, shuffle) {
-
-            $http({
-                method: 'GET',
-                url: '/phrase',
-                params: {
-                    query: phrase,
-                    max: max,
-                    start: start,
-                    shuffle: shuffle ? 'yes' : 'no'
-                }
-            }).then(function success(data) {
-                output(data.data);
-            }, function error(data) {
-                output([]);
-            });
-
-        },
-        setOutput: function(fn) {
-            output = fn;
-        },
-        customOutput: function(custom) {
-            output(custom);
-        }
-    }
-
-});

server.py
@@ -12,6 +12,7 @@ import os
 from os.path import dirname, realpath, splitext, isfile
 import sys
 import random
+from functools import cmp_to_key
 if sys.version_info[0] == 3:
     from queue import Queue, Empty
     import pickle
@@ -33,6 +34,36 @@ try:
     os.makedirs('data/cache')
 except:
     pass
+
+
+HIRAGANA_START = 0x3041
+HIRAGANA_END = 0x3096
+KATAKANA_START = 0x30A1
+KATAKANA_END = 0x30FA
+CJK_IDEO_START = 0x4e00
+CJK_IDEO_END = 0x9faf
+
+def kata_to_hira(kata):
+    hira = []
+    for k in kata:
+        if KATAKANA_START <= ord(k) <= KATAKANA_END:
+            code = ord(k)
+            code += HIRAGANA_START - KATAKANA_START
+            hira.append(chr(code))
+        else:
+            hira.append(k)
+    return ''.join(hira)
+
+def hira_to_kata(hira):
+    kata = []
+    for h in hira:
+        if HIRAGANA_START <= ord(h) <= HIRAGANA_END:
+            code = ord(h)
+            code += KATAKANA_START - HIRAGANA_START
+            kata.append(chr(code))
+        else:
+            kata.append(h)
+    return ''.join(kata)
 
 
 class Config(object):
@@ -263,6 +294,8 @@ class Dictionary(object):
 
 class JMdict_e(object):
 
+    COMMON_PRIORITY = ['news1', 'ichi1', 'spec1', 'spec2', 'gai1']
+
     def __init__(self):
         self.dictfile = open('data/JMdict_e', 'rb')
         self.temp_dictionary = dict()
@@ -271,24 +304,68 @@ class JMdict_e(object):
         self.dictionary = Dictionary(self.temp_dictionary)
         del self.temp_dictionary
 
-    def get(self, word, regex=False):
+    def get(self, word, reading=None, pos=None, regex=False):
 
         if regex:
             return self.dictionary.regex_search(word)
 
+        self._sort_reading = reading
+        self._sort_pos = set(pos) if pos else None
+
         res = self.dictionary.get(word)
 
+        key = cmp_to_key(self._sort)
+
         if res['exact']:
-            res['exact'] = [self._entry(e) for e in res['exact']]
+            res['exact'] = sorted([self._entry(e) for e in res['exact']], key=key)
         if res['shorter']:
-            res['shorter'] = [self._entry(e) for e in res['shorter']]
+            res['shorter'] = sorted([self._entry(e) for e in res['shorter']], key=key)
 
         return res
+
+    def _sort(self, a, b):
+        def common_sort(a, b):
+            common = [False, False]
+            for i, e in enumerate([a, b]):
+                common[i] = e['common']
+            if common[0] == common[1]:
+                return 0
+            return common[1] or -1
+
+        def pos_sort(a, b):
+            if not self._sort_pos:
+                return 0
+            pos_match = [False, False]
+            for i, e in enumerate([a, b]):
+                for t in e['translations']:
+                    if self._sort_pos.intersection(set([p[0] for p in t['pos']])):
+                        pos_match[i] = True
+                        break
+            if pos_match[0] == pos_match[1]:
+                return 0
+            return pos_match[1] or -1
+
+        def reading_sort(a, b):
+            if not self._sort_reading:
+                return 0
+            reading = kata_to_hira(self._sort_reading)
+            reading_match = [False, False]
+            for i, e in enumerate([a, b]):
+                for r in e['readings']:
+                    if kata_to_hira(r['text']) == reading:
+                        reading_match[i] == True
+                        break
+            if reading_match[0] == reading_match[1]:
+                return 0
+            return reading_match[1] or -1
+
+        return reading_sort(a, b) or pos_sort(a, b) or common_sort(a, b)
+
 
     def _entry(self, entry):
         ent = self.entities
 
-        entry_obj = dict(words=[], readings=[], translations=[])
+        entry_obj = dict(words=[], readings=[], translations=[], common=False)
 
         position, length = entry
 
@@ -305,6 +382,9 @@ class JMdict_e(object):
             for ke_inf in k_ele.iter('ke_inf'):
                 word['inf'].append([ke_inf.text, ent[ke_inf.text]])
             for ke_pri in k_ele.iter('ke_pri'):
+                if ke_pri.text in self.COMMON_PRIORITY:
+                    entry_obj['common'] = True
+                    word['common'] = True
                 word['pri'].append(ke_pri.text)
 
             entry_obj['words'].append(word)
@@ -316,6 +396,9 @@ class JMdict_e(object):
             for re_inf in r_ele.iter('re_inf'):
                 reading['inf'].append([re_inf.text, ent[re_inf.text]])
             for re_pri in r_ele.iter('re_pri'):
+                if re_pri.text in self.COMMON_PRIORITY:
+                    entry_obj['common'] = True
+                    reading['common'] = True
                 reading['pri'].append(re_pri.text)
             for re_restr in r_ele.iter('re_restr'):
                 reading['restr'].append(re_restr.text)
@@ -486,26 +569,6 @@ class Tatoeba(object):
         self.dictionary = dict()
         self.lines = []
         self._parse()
-
-    def search_phrase(self, phrase, max, start, shuffle):
-        lines = list(self.lines)
-        if shuffle:
-            random.shuffle(lines)
-
-        examples = []
-        counter = 0
-        while counter < start + max and lines:
-            file_pos, length = lines.pop(0)
-            self.datafile.seek(file_pos)
-            line = self.datafile.read(length).decode('utf-8')
-
-            jpn, eng = line.split('\t')[2:4]
-            if phrase in jpn:
-                if counter >= start:
-                    examples.append(dict(jpn=jpn, eng=eng))
-                counter += 1
-
-        return examples
 
     def get(self, headwords, readings):
         for hw in headwords:
@@ -692,7 +755,9 @@ class JMdict_eHandler(web.RequestHandler):
         self.set_header('Content-Type', 'application/json')
         query = self.get_query_argument('query').strip()
         regex = True if self.get_query_argument('regex', default='no') == 'yes' else False
-        response = jmdict_e.get(query, regex)
+        reading = self.get_query_argument('reading', default=None)
+        pos = self.get_query_argument('pos', default='').split(',')
+        response = jmdict_e.get(query, reading=reading, pos=pos, regex=regex)
         self.write(json.dumps(response))
 
 
@@ -714,17 +779,6 @@ class TatoebaHandler(web.RequestHandler):
         readings = self.get_query_argument('readings', default='').split(',')
         self.write(json.dumps(tatoeba.get(query, readings)))
 
-class PhraseHandler(web.RequestHandler):
-
-    def get(self):
-        self.set_header('Cache-Control', 'max-age=0')
-        self.set_header('Content-Type', 'application/json')
-        query = self.get_query_argument('query').strip()
-        max = int(self.get_query_argument('max', default=5))
-        start = int(self.get_query_argument('start', default=0))
-        shuffle = True if self.get_query_argument(
-            'shuffle', default='yes') == 'yes' else False
-        self.write(json.dumps(tatoeba.search_phrase(query, max, start, shuffle)))
 
 class KanjiVGPartsHandler(web.RequestHandler):
 
@@ -798,7 +852,6 @@ def get_app():
         (r'/jmdict_e', JMdict_eHandler),
         (r'/kanjidic2', Kanjidic2Handler),
         (r'/tatoeba', TatoebaHandler),
-        (r'/phrase', PhraseHandler),
         (r'/kvgparts', KanjiVGPartsHandler),
         (r'/kvgcombinations', KanjiVGCombinationsHandler),
         (r'/kanjisimilars', KanjiSimilarsHandler),

commit 9b01999353eae36b025287ad7c11ce62766750bd

client: fix More words starting with...
A bug introduced in d2da5287b9bf249d4bd7b7e2df8206015553ce98

client/controllers/translation.js
@@ -1,10 +1,6 @@
 angular.module('mecab-translate')
 .controller('Translation', function($scope, JMdict_e, Kanjidic2, SimilarKanji, KanjiVG, Helpers) {
 
-    $scope.queryAsString = function() {
-        return $scope.query.lemma.split('-')[0] || $scope.query.literal || $scope.query;
-    }
-
     $scope.setEntries = function(entries) {
         $scope.entries = entries;
     }

client/index.html
@@ -287,7 +287,7 @@
         </ul>
         <hr>
         <div>
-            <button ng-click="showLongerEntries()" ng-if="longerEntries.length">More words starting with {{queryAsString()}} ({{longerEntries.length}})</button>
+            <button ng-click="showLongerEntries()" ng-if="longerEntries.length">More words starting with {{(query.lemma.split('-') || [''])[0] || query.literal || query}} ({{longerEntries.length}})</button>
             <div ng-repeat="entry in longerEntryListing"><span class="clickable" ng-click="translate(entry)">{{entry}}</span></div>
         </div>
     </div>
@@ -302,7 +302,6 @@
 <script src="services/jmdict_e.js"></script>
 <script src="services/kanjidic2.js"></script>
 <script src="services/kanjivg.js"></script>
-<script src="services/tatoeba.js"></script>
 <script src="services/kanjivgparts.js"></script>
 <script src="services/similarkanji.js"></script>
 <script src="services/responsivevoice.js"></script>