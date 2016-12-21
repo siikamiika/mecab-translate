8ca7bd19b0e0cb1e3406888ba632fb1581ca2904
siikamiika <siikamiika@users.noreply.github.com>
2016-12-21 19:09:43 +0200

    downgrade angular back to 1.5.5
    1.6.0 broke SVG for some reason

--- download_dependencies.py
+++ download_dependencies.py
@@ -15,7 +15,7 @@ KANJIDIC2 = 'http://www.edrdg.org/kanjidic/kanjidic2.xml.gz'
 RADKFILE = 'http://ftp.monash.edu.au/pub/nihongo/radkfile.gz'
 TATOEBA = 'http://tatoeba.org/files/downloads/wwwjdic.csv'
 KANJIVG = 'https://github.com/KanjiVG/kanjivg/releases/download/r20160426/kanjivg-20160426-main.zip'
-ANGULAR = 'https://ajax.googleapis.com/ajax/libs/angularjs/1.6.0/angular.min.js'
+ANGULAR = 'https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.min.js'
 
 
 def download(url, destination, compression=None):
