siikamiika/mecab-translate

commit 247a705369e03c8bb93d95989fd701e9706028d4

non-click mode: show results under word

client/controllers/output.js
@@ -40,8 +40,11 @@ angular.module('mecab-translate')
         Config.set('non-click-mode', !nonclick);
     });
 
-    $scope.pauseNonClickMode = function() {
+    $scope.pauseNonClickMode = function(word) {
         $scope.nonclick = false;
+        if (word) {
+            word.tooltip = false;
+        }
     }
 
     $scope.resumeNonClickMode = function() {
@@ -119,15 +122,35 @@ angular.module('mecab-translate')
         }
     });
 
+    $scope.showTooltip = function(word, event) {
+        if (!word.tooltip && $scope.nonclick) {
+            var bodyRect = document.body.getBoundingClientRect();
+            var element = event.target.classList.contains('word') ? event.target : event.target.parentElement;
+            var rect = element.getBoundingClientRect();
+            word.right = rect.width + (bodyRect.right - rect.right);
+            word.width = Math.min(Math.max(300, bodyRect.width / 2), bodyRect.width * 0.95);
+            word.tooltip = true;
+        }
+    }
+
+    $scope.hideTooltip = function(word) {
+        if ($scope.nonclick) {
+            word.tooltip = false;
+        }
+    }
+
+    EventBridge.addEventListener('kanjidic2-tooltip', function(response) {
+        $scope.kanjidicInfo = response;
+    });
+
     $scope.clearKanjiInfoQueue = function() {
         kanjiInfoQueue = null;
     }
     var kanjiInfoQueue;
     var kanjiInfoTimer;
-    $scope.getKanjiInfo = function(kanji, mouseover) {
+    $scope.getKanjiInfo = function(kanji, mouseover, tooltip) {
         if (mouseover) {
-            if (!Helpers.isKanji(kanji))
-                return;
+            $scope.kanjidicInfo = {};
             kanjiInfoQueue = kanji;
             if (!$scope.nonclick)
                 return;
@@ -136,23 +159,30 @@ angular.module('mecab-translate')
             }
             kanjiInfoTimer = setTimeout(function() {
                 if (kanjiInfoQueue) {
-                    $scope.getKanjiInfo(kanjiInfoQueue);
+                    $scope.getKanjiInfo(kanjiInfoQueue, false, true);
                 }
             }, 100);
         } else {
-            KanjiVG.get(kanji);
-            Kanjidic2.get(kanji);
-            SimilarKanji.get(kanji);
+            if (!tooltip) {
+                KanjiVG.get(kanji);
+                SimilarKanji.get(kanji);
+            }
+            Kanjidic2.get(kanji, null, tooltip);
         }
     }
+
+    EventBridge.addEventListener('jmdict-tooltip', function(output) {
+        $scope.jmdictEntries = output.exact || [];
+    });
 
     $scope.clearWordInfoQueue = function() {
         wordInfoQueue = null;
     }
     var wordInfoQueue;
     var wordInfoTimer;
-    $scope.showWordInfo = function(word, mouseover, position) {
+    $scope.showWordInfo = function(word, mouseover, position, tooltip) {
         if (mouseover) {
+            $scope.jmdictEntries = [];
             wordInfoQueue = word;
             if (!$scope.nonclick)
                 return;
@@ -161,13 +191,15 @@ angular.module('mecab-translate')
             }
             wordInfoTimer = setTimeout(function() {
                 if (wordInfoQueue) {
-                    $scope.showWordInfo(wordInfoQueue, false, position);
+                    $scope.showWordInfo(wordInfoQueue, false, position, true);
                 }
             }, 100);
         } else if (!window.getSelection().toString()) {
-            $scope.word = word;
+            if (!tooltip) {
+                $scope.word = word;
+            }
             JMdict_e.translate(word, true, false, position && $scope.contextBasedSearch
-                ? {lines: $scope.lines, pos: position} : null);
+                ? {lines: $scope.lines, pos: position} : null, tooltip);
         }
     }
 

client/controllers/translation.js
@@ -1,5 +1,5 @@
 angular.module('mecab-translate')
-.controller('Translation', function($scope, JMdict_e, Kanjidic2, SimilarKanji, KanjiVG, Helpers) {
+.controller('Translation', function($scope, JMdict_e, Kanjidic2, SimilarKanji, KanjiVG, Helpers, EventBridge) {
 
     $scope.setEntries = function(entries) {
         $scope.entries = entries;
@@ -10,7 +10,7 @@ angular.module('mecab-translate')
         $scope.longerEntries = [];
     }
 
-    JMdict_e.setOutput(function(output) {
+    EventBridge.addEventListener('jmdict-response', function(output) {
         $scope.query = JMdict_e.getLast();
         $scope.longerEntryListing = [];
         $scope.regexResults = [];

client/index.html
@@ -155,21 +155,49 @@
                 ng-mouseleave="mouseInside = false"
                 ng-init="ttsRow = $index">
                 <div class="clickable tts-button unobtrusive" ng-click="TTS(line, true); updateRow(ttsRow)" ng-class="mouseInside ? '' : 'hide'">[TTS]</div>
-                <div class="line" ng-class="'row-' + $index">
-                    <span
+                <div class="line" ng-class="'row-' + $index"><!--
+                 --><div
                         class="clickable word"
                         ng-repeat="word in line"
                         ng-class="posClass(word.pos)"
-                        ng-attr-title="{{posClass(word.pos)}}"
                         ng-click="showWordInfo(word, null, [$parent.$index, $index])"
                         ng-mouseenter="showWordInfo(word, true, [$parent.$index, $index])"
-                        ng-mouseleave="clearWordInfoQueue()"><span
+                        ng-mouseleave="clearWordInfoQueue(); hideTooltip(word)"><!--
+                     --><div
                             class="character"
                             ng-repeat="char in word.literal track by $index"
-                            ng-click="getKanjiInfo(char); pauseNonClickMode();"
-                            ng-mouseenter="getKanjiInfo(char, true)"
-                            ng-mouseleave="clearKanjiInfoQueue()">{{char}}</span></span>
-                </div>
+                            ng-click="getKanjiInfo(char); pauseNonClickMode(word);"
+                            ng-mouseenter="getKanjiInfo(char, true); showTooltip(word, $event)"
+                            ng-mouseleave="clearKanjiInfoQueue()"><!--
+                         -->{{char}}<!--
+                     --></div><!--
+                     --><div
+                            class="word-tooltip"
+                            ng-style="{'left': (word.right < word.width ? word.right - word.width : 0) + 'px', 'width': word.width + 'px'}"
+                            ng-click="$event.stopPropagation()"
+                            ng-mouseenter="hideTooltip(word)"
+                            ng-if="word.tooltip">
+                            <div ng-if="word.reading">{{word.reading}}</div>
+                            <hr ng-if="word.reading">
+                            <div ng-class="{'hide': !kanjidicInfo.literal}">{{kanjidicInfo.literal}}: <span ng-repeat="meaning in kanjidicInfo.meaning">{{meaning}}<span class="unobtrusive" ng-if="!$last"> | </span></span></div>
+                            <hr>
+                            <div ng-repeat="entry in jmdictEntries">
+                                <div style="font-size: 18px;">
+                                    <span ng-class="{'common': word.common}" ng-repeat="word in entry.words">{{word.text}}<span ng-if="!$last">, </span></span>
+                                    <span ng-if="entry.words.length">(</span><span ng-class="{'common': reading.common}" ng-repeat="reading in entry.readings">{{reading.text}}<span ng-if="!$last">, </span></span><span ng-if="entry.words.length">)</span>
+                                </div>
+                                <div ng-repeat="translation in entry.translations">
+                                    <span>{{$index + 1}}.</span>
+                                    <span ng-repeat="pos in translation.pos"><span class="part-of-speech">{{pos[0]}}</span><span ng-if="!$last">, </span></span>
+                                    <span ng-repeat="gloss in translation.gloss" class="translation">
+                                        <span>{{gloss}}</span><span ng-if="!$last" class="unobtrusive"> | </span>
+                                    </span>
+                                </div>
+                                <hr ng-if="!$last">
+                            </div>
+                        </div><!--
+                 --></div><!--
+             --></div>
             </div>
         </div>
         <div style="font-size: 10px;" class="unobtrusive" ng-class="{'hide': !nonclick}">Non-click mode activated.</div>

client/services/config.js
@@ -10,7 +10,7 @@ angular.module('mecab-translate')
     config['show-mecab-info'] = true;
     config['show-kanji-info'] = true;
     config['show-kanji-part-browser'] = true;
-    config['non-click-mode'] = false;
+    config['non-click-mode'] = true;
     config['context-based-search'] = false;
     config['output-line-max-length'] = '';
     config['output-font-size'] = 36;

client/services/jmdict_e.js
@@ -1,12 +1,13 @@
 angular.module('mecab-translate')
-.factory('JMdict_e', function($http, Helpers) {
+.factory('JMdict_e', function($http, Helpers, EventBridge) {
 
-    var output;
     var last;
 
     return {
-        translate: function(word, obj, regex, context) {
-            last = word;
+        translate: function(word, obj, regex, context, tooltip) {
+            if (!tooltip) {
+                last = word;
+            }
             var reading, pos;
             if (obj) {
                 reading = word.lemma_reading;
@@ -61,13 +62,10 @@ angular.module('mecab-translate')
                     context: wordContext
                 }
             }).then(function success(data) {
-                output(data.data);
+                EventBridge.dispatch(tooltip ? 'jmdict-tooltip' : 'jmdict-response', data.data);
             }, function error(data) {
-                output([]);
+                EventBridge.dispatch(tooltip ? 'jmdict-tooltip' : 'jmdict-response', []);
             });
-        },
-        setOutput: function(fn) {
-            output = fn;
         },
         getLast: function() {
             return last;

client/services/kanjidic2.js
@@ -2,18 +2,18 @@ angular.module('mecab-translate')
 .factory('Kanjidic2', function($http, EventBridge) {
 
     return {
-        get: function(val, mouseover) {
+        get: function(val, mouseover, tooltip) {
             $http({
                 method: 'GET',
                 url: '/kanjidic2',
                 params: {query: val}
             }).then(function success(data) {
                 if (!mouseover || mouseover() == val) {
-                    EventBridge.dispatch('kanjidic2-response', data.data);
+                    EventBridge.dispatch(tooltip ? 'kanjidic2-tooltip' : 'kanjidic2-response', data.data);
                 }
             }, function error(data) {
                 if (!mouseover || mouseover() == val) {
-                    EventBridge.dispatch('kanjidic2-response', {});
+                    EventBridge.dispatch(tooltip ? 'kanjidic2-tooltip' : 'kanjidic2-response', {});
                 }
             });
         }

client/stylesheets/client.css
@@ -128,14 +128,25 @@ ul {
 
 .word {
     margin: 4px;
+    display: inline-block;
+    position: relative;
 }
 
 .word:hover {
     background-color: #bbb;
 }
 
-.character {
+.word-tooltip {
+    background-color: white;
+    position: absolute;
+    z-index: 1;
+    font-size: 14px;
+    cursor: initial;
+    border-style: solid;
+}
 
+.character {
+    display: inline-block;
 }
 
 .abbr:after {

commit 2f7609c634f89cc256441f527e5520c757b78127

text selection: fix on firefox
Broken in 247a705369e03c8bb93d95989fd701e9706028d4

Firefox insists on adding newlines between divs in selected text. This
fixes jmdict translation and text-to-speech, but something else has to
be done to make it possible to right-click and search from output again.

client/controllers/output.js
@@ -194,7 +194,7 @@ angular.module('mecab-translate')
                     $scope.showWordInfo(wordInfoQueue, false, position, true);
                 }
             }, 100);
-        } else if (!window.getSelection().toString()) {
+        } else {
             if (!tooltip) {
                 $scope.word = word;
             }
@@ -204,7 +204,7 @@ angular.module('mecab-translate')
     }
 
     $scope.translateSelection = function() {
-        var selection = window.getSelection().toString();
+        var selection = window.getSelection().toString().replace(/[\r\n]/g, '');
         if ($scope.lastSelection == selection)
             return;
         else
@@ -216,7 +216,7 @@ angular.module('mecab-translate')
     }
 
     $scope.updateWordLookup = function() {
-        var selection = window.getSelection().toString();
+        var selection = window.getSelection().toString().replace(/[\r\n]/g, '');
         if (selection)
             EventBridge.dispatch('text-selected', selection);
     }