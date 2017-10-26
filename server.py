3349d2470ceeb8ba7ed45e9a4d3c8c5eaa2c2dfd
siikamiika <siikamiika@users.noreply.github.com>
2017-10-27 02:03:35 +0300

    add empty mecabrc for portable executables

--- server.py
+++ server.py
@@ -179,7 +179,7 @@ class Mecab(object):
 
     def __init__(self):
         self.dictionary_format = Mecab.UNIDIC
-        self.args = ['mecab', '-d', 'data/unidic-mecab-translate']
+        self.args = ['mecab', '-d', 'data/unidic-mecab-translate', '-r', 'mecabrc']
         self.process = Popen(self.args, stdout=PIPE, stdin=PIPE, bufsize=1)
         self.output = Queue()
         self.t = Thread(target=self._handle_stdout)
