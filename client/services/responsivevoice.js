1c3d9a84f85391f1499d8688aaaff71b98f5f930
siikamiika <siikamiika@users.noreply.github.com>
2019-05-19 14:00:55 +0300

    fix responsivevoice url
    The default voice used to be female

--- client/services/responsivevoice.js
+++ client/services/responsivevoice.js
@@ -3,7 +3,7 @@ angular.module('mecab-translate')
 
     return {
         TTS: function(text) {
-            var url = 'https://code.responsivevoice.org/getvoice.php?t='+encodeURIComponent(text)+'&tl=ja&sv=&vn=&pitch=0.5&rate=0.5&vol=1';
+            var url = 'https://code.responsivevoice.org/getvoice.php?t='+encodeURIComponent(text)+'&tl=ja&sv=g1&vn=&pitch=0.5&rate=0.5&vol=1&gender=female';
             new Audio(url).play();
         }
     }
